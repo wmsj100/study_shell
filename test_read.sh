634bbcb5bf9b6967017447d43003721085c26496
wmsj100 <wmsj100@hotmail.com>
2019-04-22 23:57:46 +0800

    read

---
+++ test_read.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# test_read.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+echo "what is your name?"
+read PERSON
+echo "hello $PERSON"
