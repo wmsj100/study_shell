47bb93a9d3afb0c10c14ba93fdcc55c47a1d4ef7
wmsj100 <wmsj100@hotmail.com>
2019-05-02 09:22:20 +0800

    source . more file

---
+++ more_file/basic.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# basic.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+version=1.1.0
+name=wmsj100
+path=/home/pi/Github/Study_Shell
+course=source
---
+++ more_file/file1.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+# file1.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+. ./basic.sh
+echo $version
+echo $path
+echo $name
+echo "cur file is $0"
