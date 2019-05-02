0b6243f91c4618ca2879c2dfb30978b69dbe32f0
wmsj100 <wmsj100@hotmail.com>
2019-05-02 09:41:22 +0800

    exec

---
+++ test_exec.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# test_exec.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+echo "hello"
+exec wall "this is a broadcase"
+echo "exit over"
