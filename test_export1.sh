fcc983ed2e8e19cbbdf8cfd5dea562490e8fff7f
wmsj100 <wmsj100@hotmail.com>
2019-05-02 09:58:03 +0800

    export

---
+++ test_export1.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# test_export1.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+export a=12
+b=13
+
+sh ./test_export2.sh
---
+++ test_export2.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# test_export2.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+echo "$a"
+echo "$b"
