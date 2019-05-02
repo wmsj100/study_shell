64846db11c5220939a5f631a96dee6a844dc22c7
wmsj100 <wmsj100@hotmail.com>
2019-05-02 10:13:27 +0800

    printf

---
+++ test_printf.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# test_printf.sh
+# Copyright (C) 2019 pi <pi@raspberrypi>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+printf "the string is %s\nthe num is %d" "hello world" $((1+21))
